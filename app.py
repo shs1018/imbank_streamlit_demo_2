bce1c999974c6aaffe1f5c6a4435888340122ba8
campus3 S031 <s031@alpacoedu.onmicrosoft.com>
2024-10-17 10:29:07 +0900

    wow

---
+++ app.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+def main():
+    st.title("Let's get down to business!")
+
+    st.title("_Streamlit_ is :blue[cool] :sunglasses:")
+
+_LOREM_IPSUM = """
+Lorem ipsum dolor sit amet, **consectetur adipiscing** elit, sed do eiusmod tempor
+incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+"""
+
+def stream_data():
+    for word in _LOREM_IPSUM.split(" "):
+        yield word + " "
+        time.sleep(0.02)
+
+    yield pd.DataFrame(
+        np.random.randn(5, 10),
+        columns=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"],
+    )
+
+    for word in _LOREM_IPSUM.split(" "):
+        yield word + " "
+        time.sleep(0.02)
+
+if __name__ == "__main__":
+    main()
