edf3c934b968b07786c9570f88ad968a07a34055
campus3 S031 <s031@alpacoedu.onmicrosoft.com>
2024-10-17 15:27:00 +0900

    Multiselect update

--- app5.py
+++ app5.py
@@ -2,3 +2,25 @@
 import streamlit as st
 import pandas as pd
 import seaborn as sns
+
+@st.cache_data
+def load_data():
+    df = sns.load_dataset('penguins')
+    return df
+
+def main():
+    st.title('SelectBox 사용')
+
+    pg = load_data()
+    st.markdown('##Raw Data')
+    st.dataframe(pg)
+
+    st.markdown("<hr>", unsafe_allow_html = True)
+    st.markdown("## MultiSelect")
+
+    cols = st.multiselect("복수 칼럼 선택", pg.columns)
+    st.write("선택된 칼럼: ", cols)
+    st.dataframe(pg.loc[:, cols])
+
+if __name__ == "__main__":
+    main()
